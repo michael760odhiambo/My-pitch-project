c1a9d95061d940ddb534dd31096c682e0d6fe412
michael760odhiambo <michaelodhiambo760@gmail.com>
2019-09-19 00:25:02 +0300

    save my fifth changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,58 @@
 from flask import Flask
-from .forms import RegistrationForm,LoginForm
+from .config import config_options
 from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
+from flask_bootstrap import Bootstrap
+import secrets
 
-# Initializing application
-app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 
 
-app.config['SECRET_KEY'] = 'a76ee8997a7788e1aec336d50d604b3b'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
 
-from app import views
+db = SQLAlchemy()
+photos = UploadSet('photos',IMAGES)
+mail = Mail()
+bootstrap = Bootstrap()
+
+def create_app(config_name):
+
+    app = Flask(__name__)
+
+        # configure UploadSet
+    configure_uploads(app,photos)
+
+    # Creating the app configurations
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
+
+    app.config.from_object(config_options[config_name])
+    config_options[config_name].init_app(app)
+
+    # Initializing flask extensions
+    
+    db.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+    bootstrap.init_app(app)
+
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+
+
+    # setting config
+    app.config['SECRET_KEY']= '73a5487ab8399681a1dbef1a8f054029'
+    
+
+
+
+
+    return app
--- app/models.py
+++ app/models.py
@@ -1,25 +1,50 @@
-from app import db
-from datetime import datetime
+from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+class PhotoProfile(db.Model):
+    __tablename__ = 'profile_photos'
 
-class User(db.Model):
-    id = db.Column(db.Integer,primary_key=True)
-    username = db.Column(db.String(20),unique=True, nullable=False)
-    email = db.Column(db.String(120),unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
-    password = db.Column(db.String(50), nullable=False)
-    posts = db.relationship('Post',backref='author', lazy=True)
+    id = db.Column(db.Integer,primary_key = True)
+    pic_path = db.Column(db.String())
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+
+
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+
+    password_hash = db.Column(db.String(255))
+    photos = db.relationship('PhotoProfile',backref = 'user',lazy = "dynamic")
+    @property
+    def password(self):
+        raise AttributeError('You cannnot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.password_hash,password)
 
     def __repr__(self):
-        return f"User('{self.username}','{self.email}','{self.image_file}')"
-
-class Post(db.Model):
-     id = db.Column(db.Integer,primary_key=True)
-     title = db.Column(db.String(20), nullable=False)
-     content =  db.Column(db.Text, nullable=False)
-     date_posted =  db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-     user_id = db.Column(db.Integer,db.ForeignKey('user.id'),nullable=False)
-
-     def __repr__(self):
-            return f"Post('{self.title}','{self.date_posted}')"
+        return f'User {self.username},{self.email},{self.bio},{self.profile_pic_path}'
 
 
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role',lazy="dynamic")
+    user_id = db.Column(db.Integer,db.ForeignKey('user.id'),nullable=False)
+
+    def __repr__(self):
+        return f'User {self.name}'
